269fa22510e1ea56d615f065de8851bf6deb4250
Alan.Wang <alan@corp.makshi.com>
2018-04-13 16:16:25 +0800

    增加默认配置

---
+++ src/main/java/com/makshi/framework/mainframe/MainframeApplication.java
@@ -0,0 +1,13 @@
+package com.makshi.web.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+public class MainframeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
