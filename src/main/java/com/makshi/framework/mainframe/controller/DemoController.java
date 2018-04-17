ab4d1c2d8869d8816c4a95498be94fca1b569847
Alan.Wang <alan@corp.makshi.com>
2018-04-17 19:55:51 +0800

    a

--- src/main/java/com/makshi/framework/mainframe/controller/DemoController.java
+++ src/main/java/com/makshi/framework/mainframe/controller/DemoController.java
@@ -1,10 +1,10 @@
 package com.makshi.framework.mainframe.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class DemoController {
 
     @RequestMapping("/demo")
@@ -14,7 +14,7 @@ public class DemoController {
     
     @RequestMapping("/login.ht")
 	public ModelAndView edit() {
-		ModelAndView mv = new ModelAndView("/view/login.jsp");
+		ModelAndView mv = new ModelAndView("login");
 		return mv;
 	}
 }
