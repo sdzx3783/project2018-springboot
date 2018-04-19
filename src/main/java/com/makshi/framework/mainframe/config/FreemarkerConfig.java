302e6850b0cdf0a02f551e71835b738d9a6bd13a
sammy <sammy@corp.makshi.com>
2018-04-19 19:11:13 +0800

    修改

--- src/main/java/com/makshi/framework/mainframe/config/FreemarkerConfig.java
+++ src/main/java/com/makshi/framework/mainframe/config/FreemarkerConfig.java
@@ -1,8 +1,12 @@
 package com.makshi.framework.mainframe.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
+
+import com.hotent.core.engine.FreemarkEngine;
 
 @Configuration
 public class FreemarkerConfig {
@@ -15,4 +19,15 @@ public class FreemarkerConfig {
         freeMarkerConfigurationFactoryBean.setDefaultEncoding("UTF-8");
         return freeMarkerConfigurationFactoryBean;
     }
+    
+    
+    @Bean(value = "freemarkEngine")
+    public FreemarkEngine getFreemarkEngine(@Qualifier("freemarkerConfiguration") FreeMarkerConfigurationFactoryBean freemarkerConfiguration)
+    {
+    	FreemarkEngine freemarkEngine= new FreemarkEngine();
+    	freemarkEngine.setConfiguration();
+    	
+        return freemarkEngine;
+    }
+    
 }
