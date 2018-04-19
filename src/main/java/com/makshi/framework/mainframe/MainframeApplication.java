1c5bdff36de5cbd515abc5985324a8104992fc26
Alan.Wang <alan@corp.makshi.com>
2018-04-19 18:16:51 +0800

    改加application的路径

--- src/main/java/com/makshi/framework/mainframe/MainframeApplication.java
+++ src/main/java/com/makshi/framework/mainframe/MainframeApplication.java
@@ -1,9 +1,11 @@
-package com;
+package com.makshi.framework.mainframe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.hotent","com.makshi"})
 public class MainframeApplication{
 
     public static void main(String[] args) {
