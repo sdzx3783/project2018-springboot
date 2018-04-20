c27c71ea5080a0a57069d4aaf820027c0d92ed7b
Alan.Wang <alan@corp.makshi.com>
2018-04-20 14:43:27 +0800

    去除spring session

--- src/main/java/com/makshi/framework/mainframe/config/SpringSessionConfig.java
+++ src/main/java/com/makshi/framework/mainframe/config/SpringSessionConfig.java
@@ -1,8 +1,6 @@
 package com.makshi.framework.mainframe.config;
 
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-
-@EnableJdbcHttpSession
+//@EnableJdbcHttpSession
 public class SpringSessionConfig {
 //    @Bean
 //    public HttpSessionStrategy httpSessionStrategy() {
